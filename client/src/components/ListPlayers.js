5e0d5d0382397a01b1435207cbd86d692c86d672
Pete James <petejames@petes-air.lan>
2020-01-25 09:19:53 -0800

    component structure setup

---
+++ client/src/components/ListPlayers.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+
+export default function ListPlayers(){
+
+    console.log('from ListPlayers' );
+
+    return (
+        <div>
+            <h1>List of Players</h1>
+
+
+        </div>
+    );
+}
